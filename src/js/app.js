9965eb33ad57e241afa61675b48a6493ff87f53d
Ivan Hroshev <grosheff.ivan@gmail.com>
2022-07-14 19:47:18 +0300

    finished popups

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,13 @@
 /* import "./modules/onlyNumber.js";
 import "./modules/promoVideo.js"; */
-import "./modules/popup.js"; 
+import "./modules/ScrollTo.js";
+import "./modules/popup.js";
+import "./modules/regularExpressions.js"; 
 import "./modules/date.js";
 /* 
 import "./modules/burger.js";
-import "./modules/ScrollTo.js";
 import "./modules/geolocation.js";
 import "./modules/productSell.js";
 import "./modules/circleProgressBar.js";
 import "./modules/featureProducts.js";
-import "./modules/date.js";
-*/
-
-//Swiper
-/*
-import Swiper from "swiper/bundle";
-
-const swiper = new Swiper(".mySwiper", {
-});
 */
