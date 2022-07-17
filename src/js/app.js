afd355106ac2c9407261eeb56b32d3a5555f9c87
unknown <grosheff.ivan@gmail.com>
2022-07-17 17:24:28 +0300

    Finish work

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,5 @@
-/* import "./modules/onlyNumber.js";
-import "./modules/promoVideo.js"; */
 import "./modules/ScrollTo.js";
 import "./modules/popup.js";
 import "./modules/regularExpressions.js"; 
 import "./modules/date.js";
-/* 
-import "./modules/burger.js";
-import "./modules/geolocation.js";
-import "./modules/productSell.js";
-import "./modules/circleProgressBar.js";
-import "./modules/featureProducts.js";
-*/
+
--- src/js/modules/regularExpressions.js
+++ src/js/modules/regularExpressions.js
@@ -1,18 +1,9 @@
-//regular expression for mobile
-/* const tel = document.querySelectorAll('#tel');
-for(let i = 0; i < tel.length; i++){
-    const phone = tel[i];
-    console.log(phone)
-    phone.oninput = function(){
-        this.value = '+' + this.value.replace(/\D/g,'').substr(0,13)
-    }    
-} */
-
 //input only number
 const onlyNumber = document.querySelectorAll("#onlyNumber");
 for (let i = 0; i < onlyNumber.length; i++) {
   const number = onlyNumber[i];
+
   number.oninput = function () {
-    this.value = "+" + this.value.replace(/\D/g, "").substr(0, 13);
+      this.value = this.value.replace(/\D/g, "").substr(0, 13);
   };
 }
